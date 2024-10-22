160ef4643942b5d60e50d0ac9a51b0ee70cdb61b
Jens Krefeldt <jens.krefeldt@smavoo.com>
2024-10-22 17:21:33 +0200

    touch file to check issue with Github Action if it could be temporary glitch

--- jest.config-coverage.ts
+++ jest.config-coverage.ts
@@ -9,7 +9,7 @@ export default async (): Promise<Config.InitialOptions> => {
     coverageReporters: ["json-summary", "html", "lcov", "lcovonly", "text"],
     coverageThreshold: {
       global: {
-        statements: 99,
+        statements: 100,
         branches: 100,
         functions: 100,
         lines: 100,
